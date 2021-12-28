1b1ca350004c2bd38d9730134207b5a95c790b7e
Taixing Bi <taixingbi@taixings-mbp.lan>
2021-12-28 11:45:52 -0500

    first commit

---
+++ check.py
@@ -0,0 +1,9 @@
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, LSTM
+
+import tensorflow as tf
+tf.__version__
+tf.config.list_physical_devices()
+
+
+print("done")
